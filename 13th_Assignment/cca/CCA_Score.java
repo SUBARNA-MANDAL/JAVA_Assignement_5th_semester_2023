9bdd041a539971f06980f1008fce3ef12687948c
Subarna Mandal <subarnamandal17@gmail.com>
2024-01-08 23:29:32 +0530

    13th assignment still incurrect

---
+++ 13th_Assignment/cca/CCA_Score.java
@@ -0,0 +1,6 @@
+package cca;
+
+public interface CCA_Score{
+    int CCA_credit = 5;
+    void show_CCA_cred();
+}
---
+++ 13th_Assignment/student/Student.java
@@ -0,0 +1,14 @@
+package student;
+
+public class Student {
+    String Name;
+    double rollno;
+    public void put_all(String na,double roll){
+        Name = na;
+        rollno = roll;
+    }
+    public void get_all(){
+        System.out.println("The Student name is : "+Name);
+        System.out.println("Roll no : "+rollno);
+    }
+}
